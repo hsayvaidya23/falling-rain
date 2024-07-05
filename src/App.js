b7b2fad48f2312c12837927f71834ae7f3e8fa5e
hsayvaidya23 <bobbynaldocr7@gmail.com>
2024-07-05 18:03:02 +0530

    changed color gradient

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 
 import React from 'react';
 import './App.css';
-import Grid3 from './components/Grid3';
-import Grid2 from './components/Grid2';
+import Grid from './components/Grid';
 
 function App() {
   return (
     <div className="App">
       <h1>Rain Pattern</h1>
-      {/* <Grid3 cellSize={40}/> */}
-      <Grid2 cellSize={40} />
+      <Grid cellSize={40}/>
     </div>
   );
 }
 
-export default App;
+export default App
